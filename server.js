1341f251baaeaf3a2704d04d6ac16a416b10cbbb
CloudyLondon <jovialcty@gmail.com>
2022-02-26 10:44:50 -0800

    add server.js

---
+++ server.js
@@ -0,0 +1,28 @@
+//connect to database
+const { prompt } = require("inquirer");
+require("console.table"); //look up console.table
+const connection = require("./db/connection");
+
+//THEN I am presented with the following options:
+// 1. view all departments,
+// 2. view all roles,
+// 3. view all employees,
+// 4. add a department,
+// 5. add a role,
+// 6. add an employee,
+// 7. and update an employee role
+// need the 7 functions
+
+function viewDepartments() {}
+
+function viewRoles() {}
+
+function viewEmployees() {}
+
+function addDepartment() {}
+
+function addRole() {}
+
+function addEmployee() {}
+
+function updateEmployeeRole() {}
